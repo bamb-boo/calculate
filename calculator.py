c3bec5e49685c25a88a5579067a3f217b947349f
bamb-boo <gsbarathraam18@gmail.com>
2026-06-30 19:35:48 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,59 @@
+import math
+from math import sin, cos, tan, pi
+list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", ".", "sin", "cos", "tan", "pi", "equals"]
+numbers=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+evaluate=""
+choices=[]
+done=False
+velocity= 14.008 # set it to 14.69081 later
+
+while done==False:
+    angle=float(input("enter the angle "))
+    angle=math.radians(angle)
+    distance=((velocity**2)*math.sin(2*angle))/9.81
+    choice=list[math.floor(distance)-1]
+    print(choice)
+    choices.append(choice)
+    if choice=="equals":
+        done=True
+
+brackets=0
+for i in range(len(choices)-1):
+    value=choices[i]
+    if value in ["+", "-", "*", "/"]:
+        if brackets>0:
+            evaluate=evaluate + ")"
+            brackets-=1
+        evaluate=evaluate+str(value)
+    elif value in ["sin", "cos", "tan"]:
+        evaluate=evaluate+str(value)+"("
+        brackets+=1
+    else:
+        evaluate=evaluate+str(value)
+
+evaluate=evaluate+(")"*brackets)
+print(evaluate)
+
+answer=eval(evaluate)
+print(answer)
+
+'''things to do next
+adjust for really small nos (0.1 angle etc --> negative nos)
+adjust for "human"-things such as 3sin, when we mean 3*sin (build redundancy)
+add a cannonball-like animation (maybe terminal maybe tkinter) to show ball being launched at angle and following the path through air and falling into "buckets"
+
+
+
+'''
+
+
+'''
+30
+12
+20
+7
+17
+33
+3
+45
+'''
