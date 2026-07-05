06613b3c58043ac3bc139226761d588d7ed9b399
bamb-boo <gsbarathraam18@gmail.com>
2026-07-05 12:04:55 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,21 +2,23 @@ import time as t
 import math
 from math import sin, cos, tan, pi
 list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", ".", "sin", "cos", "tan", "pi", "equals"]
+simple_list=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "+", "-", "*", "/", ".", "s", "c", "t", "π", "="]
 numbers=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 evaluate=""
 choices=[]
 done=False
 velocity= 14.008 # set it to 14.69081 later
-time=0
-y=0
-x=0
 dt=0.1
-seconddone=False
 
 while done==False:
+    time=0
+    y=0
+    x=0
+    seconddone=False
     angle=float(input("enter the angle "))
-    vx=math.cos(math.radians(angle))*velocity
-    vy=math.sin(math.radians(angle))*velocity
+    angle=math.radians(angle)
+    vx=math.cos(angle)*velocity
+    vy=math.sin(angle)*velocity
 
     distance=((velocity**2)*math.sin(2*angle))/9.81
     if 0 <= math.degrees(angle)< 1.3:
@@ -28,44 +30,66 @@ while done==False:
     choices.append(choice)
         
     while seconddone==False:
-        print("\033[2J\033[H", end="")
+        print("\033[3J\033[2J\033[H", end="")
         x=vx*time
         y=vy*time - 0.5*9.81*time**2
-        intx=int(x)
-        inty=int(y)
+        intx=round(x*4)
+        inty=int(y*2)
         time=time+dt
+        final = (y<0 and time>dt)
         for j in range(12):
             for i in range(136):
-                if i == intx and (11-j) == inty:
+                if not final and i == intx and (11-j) == inty:
                     print("*", end="")
                 else:
                     if i == 135:
                         print(" ")
                     else:
-                        print(" ",  end="")
+                        print(" ", end="")
 
         print("|  |", end="")
-        for i in range(1, 89):
+        for i in range(1, 81):
             if i%4==1:
                 print(" ", end="")
             elif i%4==2:
                 print("|", end="")
             elif i%4==3:
-                print("_", end="")
+                print(" ", end="")
             elif i%4==0:
                 print("|", end="")
         
         print("\n|  |",  end="")
-        for i in range(22):
-            print(" |_|", end="")
-        print("\n|__|")
-        if y<0 and time > dt:
-            seconddone=True
+        if final:
+            target=math.floor(distance)-1
         else:
-            print("\033[H", end="")
-            t.sleep(0.2)
+            target=intx//4
+
+        for i in range(20):
+            if final and i==target:
+                print(" |*|", end="")
+            else:
+                print(" |_|", end="")
+
+
+        print("\n|__|", end="")
+
+        for i in range(len(simple_list)-1):
+            print(f"  {simple_list[i]} ",  end="")
+
+        print(" = ")
+
+
+        if final:
+            seconddone = True
+        else:
+            t.sleep(0.1)
+
+
+
     if choice=="equals":
         done=True
+    else:
+        seconddone=False
 
 brackets=0
 for i in range(len(choices)-1):
@@ -89,14 +113,6 @@ print(evaluate)
 
 answer=eval(evaluate)
 print(answer)
-
-
-
-
-'''things to do next
-add a cannonball-like animation (maybe terminal maybe tkinter) to show ball being launched at angle and following the path through air and falling into "buckets"
-'''
-
 
 '''
 30
